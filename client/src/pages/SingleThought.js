8ce6a781438c60174981f39007f0eb2e9ad9479d
DYoshizawa079 <daniely079@gmail.com>
2022-03-25 18:17:23 -0400

    Finished up to end of 21.4.6

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,41 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+
+  // Extract the "id" parameter from the page's URL
+  const { id: thoughtId } = useParams();
+  console.log(thoughtId); // Should display the ID of the thought that's displayed
+
+  // QUERY_THOUGHT has a parameter. The second arg of useQuery can be used to accept data that goes into the parameter.
+  // Pass data for all parameters through this second arg as an object
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}
           </span>{' '}
-          thought on createdAt
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
